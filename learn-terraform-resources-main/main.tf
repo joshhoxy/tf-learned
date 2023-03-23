5e41bbdff00a892f23129262490a4d7001d1af60
joshhoxy <whtmdghkszx@gmail.com>
2023-03-23 13:26:51 +0900

    learn-dependency

--- learn-terraform-resources-main/main.tf
+++ learn-terraform-resources-main/main.tf
@@ -10,11 +10,11 @@ provider "random" {}
 resource "random_pet" "name" {}
 
 resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
-  instance_type = "t2.micro"
-  user_data     = file("init-script.sh")
+  ami                    = "ami-a0cfeed8"
+  instance_type          = "t2.micro"
+  user_data              = file("init-script.sh")
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  
+
   tags = {
     Name = random_pet.name.id
   }
@@ -23,16 +23,16 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.name.id}-sg"
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0 
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
